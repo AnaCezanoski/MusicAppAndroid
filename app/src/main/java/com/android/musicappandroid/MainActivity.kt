a00fcc87969dbc720453fa11acdb06a7663658cb
Ana Cezanoski <juliacezanoski@gmail.com>
2024-10-03 10:16:35 -0300

    nav header e menu
    20

--- app/src/main/java/com/android/musicappandroid/MainActivity.kt
+++ app/src/main/java/com/android/musicappandroid/MainActivity.kt
@@ -3,14 +3,18 @@ package com.android.musicappandroid
 import android.content.Intent
 import android.content.pm.PackageManager
 import android.os.Bundle
+import android.view.MenuItem
 import android.widget.Toast
+import androidx.appcompat.app.ActionBarDrawerToggle
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.app.ActivityCompat
 import com.android.musicappandroid.databinding.ActivityMainBinding
+import kotlin.system.exitProcess
 
 class MainActivity : AppCompatActivity() {
 
     private lateinit var binding: ActivityMainBinding
+    private lateinit var toggle: ActionBarDrawerToggle
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -19,12 +23,27 @@ class MainActivity : AppCompatActivity() {
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
 
+        toggle = ActionBarDrawerToggle(this, binding.root, R.string.open, R.string.close)
+        binding.root.addDrawerListener(toggle)
+        toggle.syncState()
+        supportActionBar?.setDisplayHomeAsUpEnabled(true)
+
         binding.shuffleBtn.setOnClickListener {
             val intent = Intent(this@MainActivity, PlayerActivity::class.java)
             startActivity(intent)
         }
         binding.favoriteBtn.setOnClickListener {
             startActivity(Intent(this@MainActivity, FavoriteActivity::class.java))
+        }
+        binding.navView.setNavigationItemSelectedListener {
+            when(it.itemId)
+            {
+                R.id.navShuffle -> Toast.makeText(baseContext, "Shuffle", Toast.LENGTH_SHORT).show()
+                R.id.navFavorites -> Toast.makeText(baseContext, "Favorites", Toast.LENGTH_SHORT).show()
+                R.id.navPlaylist -> Toast.makeText(baseContext, "Playlist", Toast.LENGTH_SHORT).show()
+                R.id.navExit -> exitProcess(1)
+            }
+            true
         }
     }
 
@@ -48,5 +67,11 @@ class MainActivity : AppCompatActivity() {
             else
                 ActivityCompat.requestPermissions(this, arrayOf(android.Manifest.permission.WRITE_EXTERNAL_STORAGE), 13)
         }
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        if(toggle.onOptionsItemSelected(item))
+            return true
+        return super.onOptionsItemSelected(item)
     }
 }
